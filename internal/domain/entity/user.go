190a283ca59e822e9b3923b4f96b32fbcc1e0182
Yuri Oliveira <yusadeol@gmail.com>
2025-04-20 22:04:19 -0300

    feat: enhance user entity with timestamps and password value object
    - Change password field type to use Password value object
    - Add CreatedAt and UpdatedAt timestamp fields
    - Update NewUser constructor to handle new fields

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -1,19 +1,27 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/yusadeol/go-budgeteer/internal/domain/vo"
+	"time"
+)
 
 type User struct {
-	Id       string
-	Name     string
-	Email    string
-	Password string
+	Id        string
+	Name      string
+	Email     string
+	Password  *vo.Password
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
-func NewUser(name, email, password string) *User {
+func NewUser(name, email string, password *vo.Password) *User {
 	return &User{
-		Id:       uuid.New().String(),
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Id:        uuid.New().String(),
+		Name:      name,
+		Email:     email,
+		Password:  password,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
 	}
 }
