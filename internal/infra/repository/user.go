d258c7144ec6942067a17d4ae9163c3dd613152e
Yuri Oliveira <yusadeol@gmail.com>
2025-04-27 23:52:40 -0300

    refactor: improve user repository to use password hasher
    - Add passwordHasher dependency to UserDatabase
    - Update constructor to receive passwordHasher
    - Change Save and GetByEmail methods to use passwordHasher

--- internal/infra/repository/user.go
+++ internal/infra/repository/user.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"errors"
 	"github.com/yusadeol/go-budgeteer/internal/domain/entity"
+	"github.com/yusadeol/go-budgeteer/internal/domain/vo"
 )
 
 var (
@@ -16,15 +17,16 @@ type User interface {
 }
 
 type UserDatabase struct {
-	dbConnection *sql.DB
+	dbConnection   *sql.DB
+	passwordHasher vo.PasswordHasher
 }
 
-func NewUserDatabase(dbConnection *sql.DB) *UserDatabase {
-	return &UserDatabase{dbConnection: dbConnection}
+func NewUserDatabase(dbConnection *sql.DB, passwordHasher vo.PasswordHasher) *UserDatabase {
+	return &UserDatabase{dbConnection: dbConnection, passwordHasher: passwordHasher}
 }
 
-func (u *UserDatabase) Save(user *entity.User) error {
-	_, err := u.dbConnection.Exec(
+func (r *UserDatabase) Save(user *entity.User) error {
+	_, err := r.dbConnection.Exec(
 		"INSERT INTO users(id, name, email, password, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?)",
 		user.Id,
 		user.Name,
@@ -40,23 +42,27 @@ func (u *UserDatabase) Save(user *entity.User) error {
 	return nil
 }
 
-func (u *UserDatabase) GetByEmail(email string) (*entity.User, error) {
+func (r *UserDatabase) GetByEmail(email string) (*entity.User, error) {
 	var user entity.User
+	var password string
 
-	err := u.dbConnection.QueryRow(
+	err := r.dbConnection.QueryRow(
 		"SELECT id, name, email, password, created_at, updated_at FROM users WHERE email = ?",
 		email,
 	).Scan(
 		&user.Id,
 		&user.Name,
 		&user.Email,
-		&user.Password,
+		&password,
 		&user.CreatedAt,
 		&user.UpdatedAt,
 	)
 	if err != nil {
 		return nil, err
 	}
+
+	voPassword := vo.NewPasswordFromHash(r.passwordHasher, password)
+	user.Password = voPassword
 
 	return &user, nil
 }
